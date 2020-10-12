298e1ed57fcf209c19919895503d8ea6aa3a0bf9
aprilhoward <apehow111@gmail.com>
2020-10-12 16:21:00 -0400

    solution using charAt

--- src/StartOz.java
+++ src/StartOz.java
@@ -13,5 +13,15 @@ public class StartOz {
         }
         return results;
     }
+    public static String startOz2(String str) {
+        String result = "";
+        if(str.length() >= 1 && str.charAt(0) == 'o') {
+            result = result + str.charAt(0);
+        }
 
+        if(str.length() >= 2 && str.charAt(1) == 'z') {
+            result = result + str.charAt(1);
+        }
+        return result;
+    }
 }
