02ec510cc96f4e2afec500f1a80699ee722bb5ff
aprilhoward <apehow111@gmail.com>
2020-10-12 18:02:43 -0400

    wrote conditions in main

--- src/InTheRange.java
+++ src/InTheRange.java
@@ -11,4 +11,20 @@ public class InTheRange {
         }
         return false;
     }
+
+    public static void main(String[] args) {
+        System.out.println("(30, 31) should return true: " + inRange(30, 31));
+        System.out.println("(30, 41) should return false: " + inRange(30, 41));
+        System.out.println("(40, 50) should return true: " + inRange(40, 50));
+        System.out.println("(40, 51) should return false: " + inRange(40, 51));
+        System.out.println("(39, 50) should return false: " + inRange(39, 50));
+        System.out.println("(50, 39) should return false: " + inRange(50, 39));
+        System.out.println("(40, 39) should return true: " + inRange(40, 39));
+        System.out.println("(49, 48) should return true: " + inRange(49, 48));
+        System.out.println("(50, 40) should return true: " + inRange(50, 40));
+        System.out.println("(50, 51) should return false: " + inRange(50, 51));
+        System.out.println("(35, 36) should return true: " + inRange(35, 36));
+        System.out.println("(35, 45) should return false: " + inRange(35, 45));
+
+    }
 }
