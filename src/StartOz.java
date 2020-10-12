2b749cd60364a8eec38dad91d04af37361567c68
aprilhoward <apehow111@gmail.com>
2020-10-12 16:10:14 -0400

    StartOz one solution

---
+++ src/StartOz.java
@@ -0,0 +1,17 @@
+public class StartOz {
+    // Given a string, return a string made of the first 2 chars
+    // (if present), however include first char only if it is 'o'
+    // and include the second only if it is 'z', so "ozymandias"
+    // yields "oz".
+
+    public static String startOz(String str) {
+        String results = "";
+        if(str.length() >= 1 && str.substring(0, 1).equals("o")) {
+            results = str.substring(0, 1);
+        } if(str.length() >= 2 && str.substring(1, 2).equals("z")) {
+            results = results + str.substring(1, 2);
+        }
+        return results;
+    }
+
+}
