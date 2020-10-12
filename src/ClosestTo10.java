b79af5018e03ee4cc6e4f329e0a57847ca0d00f8
aprilhoward <apehow111@gmail.com>
2020-10-12 17:43:41 -0400

    tested conditions in main

--- src/ClosestTo10.java
+++ src/ClosestTo10.java
@@ -10,4 +10,21 @@ public class ClosestTo10 {
         }
         return 0;
     }
+
+    public static void main(String[] args) {
+        System.out.println("(8, 13) should return 8: " + closestTo10(8, 13));
+        System.out.println("(13, 8) should return 8: " + closestTo10(13, 8));
+        System.out.println("(13, 7) should return 0: " + closestTo10(13, 7));
+        System.out.println("(7, 13) should return 0: " + closestTo10(7, 13));
+        System.out.println("(9, 13) should return 9: " + closestTo10(9, 13));
+        System.out.println("(13, 8) should return 8: " + closestTo10(13, 8));
+        System.out.println("(10, 12) should return 10: " + closestTo10(10, 12));
+        System.out.println("(11, 10) should return 10: " + closestTo10(11, 10));
+        System.out.println("(5, 21) should return 5: " + closestTo10(5, 21));
+        System.out.println("(0, 20) should return 0: " + closestTo10(0, 20));
+        System.out.println("(10, 10) should return 0: " + closestTo10(10, 10));
+        System.out.println("(100, -50) should return -50: " + closestTo10(100, -50));
+
+
+    }
 }
