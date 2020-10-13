9a51b342516d74d4dd8cbc622bb320b8ae75e8a3
aprilhoward <apehow111@gmail.com>
2020-10-13 15:38:37 -0400

    called method in main

--- src/StringBits.java
+++ src/StringBits.java
@@ -1,5 +1,8 @@
 public class StringBits {
 
+    // Given a string, return a new string made of every other
+    // char starting with the first, so "Hello" yields "Hlo".
+
     public static String stringBits(String str) {
         String newStr = "";
         for(int i = 0; i < str.length(); i= i+2){
@@ -10,5 +13,15 @@ public class StringBits {
 
     public static void main(String[] args) {
         System.out.println("'Hello' should return 'Hlo': " + stringBits("Hello"));
+        System.out.println("'Hi' should return 'H': " + stringBits("Hi"));
+        System.out.println("'Heeololeo' should return 'Hello': " + stringBits("Heeololeo"));
+        System.out.println("'HiHiHi' should return 'HHH': " + stringBits("HiHiHi"));
+        System.out.println("'' should return '': " + stringBits(""));
+        System.out.println("'Greetings' should return 'Getns': " + stringBits("Greetings"));
+        System.out.println("'Chocoate' should return 'Coot': " + stringBits("Chocoate"));
+        System.out.println("'pi' should return 'p': " + stringBits("pi"));
+        System.out.println("'Hello Kitten' should return 'HloKte': " + stringBits("Hello Kitten"));
+        System.out.println("'hxaxpxpxy' should return 'happy': " + stringBits("hxaxpxpxy"));
+
     }
 }
